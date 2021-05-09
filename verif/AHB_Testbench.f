f3f5ef605332237e25255420f9f27acda8a7ea3e
Thuy Nguyen <57820377+tnguyen-tx@users.noreply.github.com>
2021-05-09 13:47:10 -0500

    Add files via upload

---
+++ verif/AHB_Testbench.f
@@ -0,0 +1,16 @@
+
+../rtl/AHBDecoder.v
+../rtl/AHBMuxS2M.v
+../rtl/AHBZBTRAM.v
+../rtl/AHB2APB.v
+../rtl/AHBAPBSys.v
+../rtl/APBIntcon.v
+../rtl/APBRegs.v
+../rtl/AHBAHBTop.v
+
+../beh/AMBA_declare.v
+../beh/AHB_HC_master.v
+../beh/RA1SH.v
+../beh/SRAM_8X4X4096.v
+
+../beh/AHB_Testbench.v
---
+++ verif/LM_AHBAPB.f
@@ -0,0 +1,9 @@
+../rtl/AHBDecoder.v
+../rtl/AHBMuxS2M.v
+../rtl/AHBZBTRAM.v
+../rtl/AHB2APB.v
+../rtl/AHBAPBSys.v
+../rtl/APBIntcon.v
+../rtl/APBRegs.v
+../rtl/AHBAHBTop.v
+
